e3a6f2d7ae76b0a24211b7f1e1891dd6ab63bec5
Bruk Fekade <fekadeb2@vcu.edu>
2021-04-09 14:58:17 -0400

    Adding spiralize function

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,6 +1,5 @@
 def spiralize(number):
     return_value = 1
     n = (number - 1) / 2
-    z = (3 + 2 * n * (8 * n * n + 15 * n + 13)) / 3
-    return_value = z
+    return_value = (3 + 2 * n * (8 * n * n + 15 * n + 13)) / 3
     return return_value
